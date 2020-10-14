5765b36e0934811fc53e9a04c4449941cc7fc979
youfdme <49680184+youfdme@users.noreply.github.com>
2020-10-15 00:08:09 +0800

    Update telnet.sh
    sh telnet.sh xxx.txt

--- telnet.sh
+++ telnet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for host in `cat ./host.txt`
+for host in `cat $1`
 do
  ip=`echo $host | awk 'BEGIN{FS=":"} {print $1}'`
  port=`echo $host | awk 'BEGIN{FS=":"} {print $2}'`
  sleep 0.5
- check=`echo quit | timeout --signal=9 2 telnet $ip $port 2>/dev/null | grep Escape | wc -l`
+ check=`echo quit | timeout --signal=9 1 telnet $ip $port 2>/dev/null | grep Escape | wc -l`
  if [ $check -eq 1 ];then
         echo "$ip $port is OK."
  else
