86d77c9c2ca59ef866ce68badd6e2374d4a0881d
youfdme <49680184+youfdme@users.noreply.github.com>
2020-10-26 15:43:53 +0800

    Update telnet.sh
    add output with color 2020年10月26日15:43:52

--- telnet.sh
+++ telnet.sh
@@ -3,12 +3,12 @@ for host in `cat $1`
 do
  ip=`echo $host | awk 'BEGIN{FS=":"} {print $1}'`
  port=`echo $host | awk 'BEGIN{FS=":"} {print $2}'`
- sleep 0.5
+ sleep 0.1
  check=`echo quit | timeout --signal=9 1 telnet $ip $port 2>/dev/null | grep Escape | wc -l`
  if [ $check -eq 1 ];then
-        echo "$ip $port is OK."
+	echo -e "\033[32m$ip $port is OK\033[0m"
  else
-        echo -e "\033[31m$ip $port is FAILE ....\033[0m"
+	echo -e "\033[31m$ip $port is FAILE ....\033[0m"
  fi
 
 done
